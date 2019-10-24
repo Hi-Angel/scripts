67a2d11c01d6a68513fd81f2f8804d3a790c370c
Konstantin Kharlamov <Hi-Angel@yandex.ru>
2019-10-24 14:19:21 +0300

    drugs-timer.py: add a script to calculate time for a list of drugs
    List of drugs is represented by:
    
        A name with any symbols α
    
    where α is a number of times to use it per day
    
    Signed-off-by: Konstantin Kharlamov <Hi-Angel@yandex.ru>

---
+++ drugs-timer.py
@@ -0,0 +1,21 @@
+import fileinput
+import sys
+
+average_daytime = 12
+base_time = int(input())
+assert(base_time >= 0 and base_time <= 24)
+def calc_one_drug(description_line):
+    descr_split = description_line.split()
+    n_times_a_day = int(descr_split[-1])
+    assert(n_times_a_day >= 1)
+    drug_name = " ".join(descr_split[:-1])
+    if n_times_a_day == 1:
+        return (drug_name, [base_time])
+    else:
+        optimal_timespan = int(average_daytime / (n_times_a_day-1))
+        return (drug_name, [(base_time + h * optimal_timespan) % 24 for h in range(0, n_times_a_day)])
+
+for descr in fileinput.input():
+    if not descr.isspace():
+        (drug_name, spans) = calc_one_drug(descr)
+        print(drug_name + " " + str(spans))
