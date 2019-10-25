36df6378de71acb73bfeab58509f236463b2f415
Konstantin Kharlamov <Hi-Angel@yandex.ru>
2019-10-25 19:06:03 +0300

    drugs-timer.py: change the output as "time first, name second"
    Signed-off-by: Konstantin Kharlamov <Hi-Angel@yandex.ru>

--- drugs-timer.py
+++ drugs-timer.py
@@ -5,30 +5,34 @@ from functools import reduce
 average_daytime = 15
 base_time = int(input())
 assert(base_time >= 0 and base_time <= 24)
-def calc_one_drug(description_line):
+
+# String -> {UInt, String} -> {UInt, String}
+def calc_one_drug(description_line, times_accum):
     descr_split = description_line.split()
     n_times_a_day = int(descr_split[-1])
     assert(n_times_a_day >= 1)
     drug_name = " ".join(descr_split[:-1])
-    if n_times_a_day == 1:
-        return (drug_name, [base_time])
-    else:
+    def more_than_one_time():
         optimal_timespan = int(average_daytime / (n_times_a_day-1))
-        return (drug_name, [(base_time + h * optimal_timespan) % 24 for h in range(0, n_times_a_day)])
+        return [(base_time + h * optimal_timespan) % 24 for h in range(0, n_times_a_day)]
+    times = [base_time] if n_times_a_day == 1 else more_than_one_time()
+    for time in times:
+        if time in times_accum:
+            times_accum[time] += ", " + drug_name
+        else:
+            times_accum[time] = drug_name
+    return times_accum
 
 def pretty_hour(h):
     return str(h) + "⁰⁰"
 
 def parse():
-    return [calc_one_drug(descr) for descr in fileinput.input()
-                                 if not descr.isspace()]
+    accum = {}
+    for descr in fileinput.input():
+        if not descr.isspace():
+            calc_one_drug(descr, accum)
+    return accum
 
 drugs = parse()
-drugs.sort(key = lambda v: len(v[1]))
-
-for (drug_name, spans) in drugs:
-    # align to 3 tabs = 24 spaces
-    tabs = '\t\t\t' if len(drug_name) <= 7 else \
-        '\t\t' if len(drug_name) <= 15 else \
-        '\t'
-    print(drug_name + tabs + pretty_hour(spans[0]) + reduce(lambda acc,h: acc + ", " + pretty_hour(h), spans[1:], ""))
+for (time, drug_names) in sorted(drugs.items()):
+    print(pretty_hour(time) + "\t" + drug_names)
