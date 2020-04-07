32eb1af6b89b7a82a343a7a6733cb0579b81b5c1
Konstantin Kharlamov <Hi-Angel@yandex.ru>
2020-04-07 22:18:29 +0300

    warn-on-low-memory.pl: declare variables with "my", use warnings
    Signed-off-by: Konstantin Kharlamov <Hi-Angel@yandex.ru>

--- warn-on-low-memory.pl
+++ warn-on-low-memory.pl
@@ -1,19 +1,21 @@
 #!/usr/bin/perl
 # this shows a pop up message if SWAP file has less than 25% left. For those
-# wondering, whether system is usable at that moment: well, with ZSWAP enabled on 5.3
-# and later kernel (at least with an SSD disk) there's no lags until the moment SWAP
-# is 100% full.
-$user_alerted=0;
+# wondering, whether system is usable at that moment: well, with ZSWAP enabled on
+# 5.3 and later kernel (at least with an SSD disk) there's no lags until SWAP
+# gets 100% full.
+use warnings;
+
+my $user_alerted=0;
 while (1) {
-    $free_output=`free`;
-    foreach (split(/\n/, $free_output)) {
+    my $free_output=`free`;
+    foreach (split /^/, $free_output) {
         if (/^Swap/) {
-            $total=$F[1];
+            my $total=$F[1];
             if ($total == 0) {
                 print "Swap is missing, nothing to do\n"
             } else {
-                $used=$F[2];
-                $threshold=$total*0.75;
+                my $used=$F[2];
+                my $threshold=$total*0.75;
                 if ($threshold <= $used) {
                     if (not $user_alerted) {
                         if (system('notify-send "Warning" "Your memory is low!"')) {
