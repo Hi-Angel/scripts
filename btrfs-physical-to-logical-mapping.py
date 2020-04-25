ef8cf4bb93d997e63c1b6adc75ebebbf48853234
Konstantin Kharlamov <Hi-Angel@yandex.ru>
2020-04-26 01:44:51 +0300

    btrfs-physical-to-logical-mapping.py: replace the script with knorrie's one
    Signed-off-by: Konstantin Kharlamov <Hi-Angel@yandex.ru>

--- btrfs-physical-to-logical-mapping.py
+++ btrfs-physical-to-logical-mapping.py
@@ -1,97 +1,42 @@
 #!/usr/bin/python3
 
+# Props for this script go to Hans van Kranenburg, his script using btrfs is
+# waaay shorter and more robust than mine which had been parsing output of a
+# btrfs command line utility
+
+import btrfs
 import sys
-import subprocess
 
-if len(sys.argv) != 3:
-    print("""Usage: %s dev physical_offset_inside_btrfs_partition""" % sys.argv[0])
-    exit(1)
 
-device = sys.argv[1]
-phys_offset = int(sys.argv[2])
+def get_dev_extent(fs, devid, paddr, path):
+    # we can't just simply search backwards... :|
+    tree = btrfs.ctree.DEV_TREE_OBJECTID
+    min_key = btrfs.ctree.Key(devid, btrfs.ctree.DEV_EXTENT_KEY, 0)
+    max_key = btrfs.ctree.Key(devid, btrfs.ctree.DEV_EXTENT_KEY, paddr)
+    for header, data in btrfs.ioctl.search_v2(fs.fd, tree, min_key, max_key):
+        pass
+    return btrfs.ctree.DevExtent(header, data)
 
-# data ParseState UInt = Searching | KeyFound | SizeFound UInt
-class ParseState:
-    # a hackish way to make an ADT in python
-    # state == 0 = Searching
-    # state == 1 = KeyFound
-    # state == 2 = SizeFound
-    def __init__(self, state, size = None):
-        self.state = state
-        self.size = size
 
-    def Searching():
-        return ParseState(0)
-
-    def KeyFound():
-        return ParseState(1)
-
-    def SizeFound(sz):
-        assert isinstance(sz, int)
-        return ParseState(2, sz)
-
-    def isSearching(self):
-        return self.state == 0
-
-    def isKeyFound(self):
-        return self.state == 1
-
-    def isSizeFound(self):
-        return self.state == 2
-
-class Range:
-    def __init__(self, start, past_end):
-        self.start = start
-        self.past_end = past_end
-
-# data Parser = Parser [Range] ParseState
-class Parser:
-    def __init__(self, ranges, state):
-        self.ranges = ranges
-        self.state = state
-
-# String -> [String]
-def run_cmd(cmd):
-    proc = subprocess.run(cmd.split(), encoding='utf-8', stdout=subprocess.PIPE)
-    output = [line for line in proc.stdout.split('\n')]
-    if proc.returncode != 0:
-        print(f"Command failed, exiting. Failing cmd is: {cmd}, output is {output}")
+def main():
+    if len(sys.argv) != 4:
+        print(f"""Usage:
+        {sys.argv[0]} <btrfs_dev_id> <physical_address> <btrfs_mount_point>
+        The devid can be gotten from `btrfs fi show` command""")
         exit(1)
-    return output
+    devid = int(sys.argv[1])
+    paddr = int(sys.argv[2])
+    path = sys.argv[3]
 
-def build_ranges(cmd_out, parser):
-    for line in cmd_out:
-        if parser.state.isSearching():
-            if "FIRST_CHUNK_TREE" in line:
-                parser.state = ParseState.KeyFound()
-        elif parser.state.isKeyFound():
-            if "length" in line: # it's the 1st word, the second is the number
-                parser.state = ParseState.SizeFound(int(line.split()[1]))
-            elif "FIRST_CHUNK_TREE" in line:
-                print("WRN: there was a key without the length")
-        elif parser.state.isSizeFound():
-            if "offset" in line:
-                offset = int(line.split()[-1])
-                parser.ranges.append(Range(offset, offset + parser.state.size))
-            elif "FIRST_CHUNK_TREE" in line:
-                parser.state = ParseState.KeyFound()
-        else:
-            assert False, 'BUG: unknown state'
-    return parser.ranges
+    with btrfs.FileSystem(path) as fs:
+        dev_extent = get_dev_extent(fs, devid, paddr, path)
 
-cmd_out = run_cmd(f"btrfs ins dump-tree -t 3 {device}")
-ranges = build_ranges(cmd_out, Parser([], ParseState.Searching()))
-ranges.sort(key=lambda r: r.start)
+    print("devid {} paddr {} is part of {}".format(devid, paddr, dev_extent))
+    print("DISCLAIMER: next things won't work for RAID56!!")
+    way_into = paddr - dev_extent.paddr
+    print("paddr is {} bytes into that dev extent".format(way_into))
+    print("so, logical/virtual address would be {}".format(dev_extent.chunk_offset + way_into))
 
-found = False
-for r in ranges:
-    if phys_offset >= r.start and phys_offset < r.past_end:
-        if found:
-            print("INF: this is odd, but there's more than one offset matches")
-        found = True
-        print(f"It's in range ({r.start}, {r.past_end})")
-    if phys_offset < r.start:
-        break # we sorted it by "start"
 
-if not found:
-    print("Offset was not found")
+if __name__ == '__main__':
+    main()
